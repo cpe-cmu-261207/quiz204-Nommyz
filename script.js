5c6d9dbc1d2059354f11b8dea214e9f888d5e8f7
Nommyz <76035501+Nommyz@users.noreply.github.com>
2021-07-29 14:42:16 +0700

    Bruh

--- script.js
+++ script.js
@@ -4,11 +4,31 @@ const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
-// define more constants and variables here
+const text = document.querySelector("#text");
+const deftext = text.innerHTML;
+var words = [];
+var WordColor = color.value;
+
+const searchbtn = document.querySelector("#search");
+const btnreset = document.querySelector("#reset");
+
 
 btn_toggle.onclick = () => {
-  const calculate =  document.querySelector('h2')
-  calculate.innerHTML = "630610748"
+	const btnElm = document.querySelector('h2')
+	btnElm.innerHTML = "630610748"
 }
 
-// more codes for Search and Reset buttons here
+searchbtn.onclick = () => {
+	words = deftext.split(/[ ]+/);
+	console.log(words);
+	var newtext = "";
+	for(let i = 0 ; i < words.length; ++i){
+		if(words[i].length >= parseInt(length.value)){
+			newtext += "<span style='background-color:" + color.value + "'>" + words[i] +  "</span>" + " ";
+		}else{
+			newtext += words[i] + " ";
+		}
+	}
+	text.innerHTML = newtext;
+}
+
